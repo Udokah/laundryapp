c6130324c51e765133007f8673ec0dc71f0ccb3b
Udo <udo@Monsieur-Slim.local>
2015-06-18 10:41:02 +0100

    added bower components

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Washalert
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Bower components
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
