b6e84b47ffffd01cb55f71d6ae617bb191d3baec
Udo <writeudo@gmail.com>
2015-06-21 09:14:14 +0100

    added lib directory to asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Washalert
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    # Load my modules
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # Bower components
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
   end
