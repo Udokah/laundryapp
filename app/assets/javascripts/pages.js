bbd48734f4555d3815f9f0d0ffb5bc18edf1cbfb
Udo <writeudo@gmail.com>
2015-06-21 09:13:47 +0100

    refined slack class for frontend

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,7 +1,34 @@
-$(document).ready(function(){
+"use strict";
 
+  /**
+   * Slack Class
+   */
+  var Slack = function(){
+    this.state = this.clientId = null;
+    this.uri = null;
+    this.team = this.scope = null
+  };
+
+  Slack.prototype.params = function(){
+    return {
+      client_id : this.clientId,
+      state     : this.state,
+      team      : this.team,
+      scope     : this.scope     
+    };
+  };
+
+$(document).ready(function(){
   var menu = $('.shared-menu');
   var topBanner = $('.top-banner');
+  var slackBtn = $('#slack-btn');
+
+  var slack = new Slack();
+  slack.clientId = slackBtn.attr('data-client-id');
+  slack.state = slackBtn.attr('data-state');
+  slack.team = slackBtn.attr('data-team');
+  slack.scope = slackBtn.attr('data-scope');
+  slack.uri = slackBtn.attr('data-uri');
 
   /**
    * Loop through links
@@ -13,4 +40,15 @@ $(document).ready(function(){
     }
   });
 
+  /**
+   * Slack button is clicked
+   */
+  slackBtn.on('click', function(e){
+    e.preventDefault();    
+    window.location = slack.uri + "?" + $.param(slack.params());
+  });
+
 }); 
+
+
+
