559c615c32ad924370186f6ceb55a21d9f05a111
Udo <writeudo@gmail.com>
2015-07-01 14:57:29 +0100

    make user active if person is first to book for that day

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,15 +1,21 @@
 class SchedulesController < ApplicationController
   protect_from_forgery
+  require 'pry'
 
   def create
     @schedule = Schedule.new
+    today = Schedule.where("created_at >= ?", Time.zone.now.beginning_of_day)
+    @schedule.status = "pending"
+
+    if today.length == 0 then
+      @schedule.status = "active"
+    end
+    
     @schedule.slack_id = params[:id]
     @schedule.name = params[:name]
     @schedule.picture = params[:picture]
     @schedule.slack_token = params[:token]
     @schedule.email = params[:email]
-    @schedule.status = "pending"
-
     if @schedule.save
       result = {:status => true}
     else
