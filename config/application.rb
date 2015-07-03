d527d98d957c7a320bda6412bf921d65cd7d33cb
Udo <writeudo@gmail.com>
2015-07-03 08:02:42 +0100

    bower stuff

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,20 @@ module Washalert
 
     # Bower components
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+
+    # via https://gist.github.com/afeld/5704079
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml',                 # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
+
   end
 end
