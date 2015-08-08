a2c211cbbb6f9830e120bb75934ce6535fb6da45
Udo Nkwocha <writeudo@gmail.com>
2015-08-08 09:35:35 +0100

    code refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,8 @@ class PagesController < ApplicationController
   @@slack = Slack.new
 
   def index 
-    @page_title = "LaundryApp"
-    @class = "index"
+    @page_title = 'LaundryApp'
+    @class = 'index'
     @auth_status = 'false'
     @schedule_btn_class = get_schedule_btn_class
     @host = request.host
@@ -19,20 +19,20 @@ class PagesController < ApplicationController
     if params.has_key?(:code) && params.has_key?(:state) && cookies[:state] == params[:state]
       get_token = @@slack.get_token(params[:code]) # get access token
       if get_token["ok"] then
-        auth_user = @@slack.auth_user(get_token["access_token"]) # authenticate user
+        auth_user = @@slack.auth_user(get_token['access_token']) # authenticate user
         if auth_user["ok"] then
           # get users info now
-          user_info = @@slack.get_user_info( get_token["access_token"], auth_user["user_id"] )
+          user_info = @@slack.get_user_info( get_token['access_token'], auth_user['user_id'] )
           if user_info["ok"] then
-            session[:user] = { :info  => user_info["user"], 
-             :token => get_token["access_token"], 
-             :team  => auth_user["team_id"]}
+            session[:user] = { :info  => user_info['user'],
+             :token => get_token['access_token'],
+             :team  => auth_user['team_id']}
              @auth_status = 'true'
              redirect_to '/dashboard'
            end
          end
        end
-       @authorization_href = "Javascript:void()"
+       @authorization_href = 'Javascript:void()'
      else
       @auth_status = 'true'
       @authorization_href = @@slack.get_auth_href(state_cookie)
@@ -41,12 +41,12 @@ class PagesController < ApplicationController
 
   def dashboard
     authenticate_user
-    @page_title = "Dashboard | LaundryApp"
-    @class = "dashboard"
+    @page_title = 'Dashboard | LaundryApp'
+    @class = 'dashboard'
     @user = session[:user]
     @schedule_btn_class = get_schedule_btn_class
     @host = request.base_url
-    @today = Schedule.where("created_at >= ? and status != ?", Time.zone.now.beginning_of_day, "done").order("created_at")
+    @today = Schedule.where('created_at >= ? and status != ?', Time.zone.now.beginning_of_day, 'done').order('created_at')
   end
 
   def schedule
@@ -60,7 +60,7 @@ class PagesController < ApplicationController
     @schedule_is_active = schedule_is_active?
     @todays_schedule = nil
     if @has_scheduled then
-      @todays_schedule = Schedule.where("created_at >= ? and slack_id = ? and status != ?", Time.zone.now.beginning_of_day, session[:user]["info"]["id"], "done")
+      @todays_schedule = Schedule.where('created_at >= ? and slack_id = ? and status != ?', Time.zone.now.beginning_of_day, session[:user]["info"]["id"], "done")
     end
   end
 
@@ -92,10 +92,10 @@ class PagesController < ApplicationController
   end
 
   def has_scheduled?
-    status = false;
-    if session[:user] then 
-      qry = Schedule.where("created_at >= ? and slack_id = ? and status != ?", Time.zone.now.beginning_of_day, session[:user]["info"]["id"], "done")
-      if qry.length >= 1 then
+    status = false
+    if session[:user]
+      qry = Schedule.where('created_at >= ? and slack_id = ? and status != ?', Time.zone.now.beginning_of_day, session[:user][:info]['id'], 'done')
+      if qry.length >= 1
         status = true;
       end
     end
@@ -103,7 +103,7 @@ class PagesController < ApplicationController
   end
 
   def schedule_is_active?
-    status = false;
+    status = false
     if session[:user] then 
       qry = Schedule.where("created_at >= ? and slack_id = ? and status = ?", Time.zone.now.beginning_of_day, session[:user]["info"]["id"], "active")
       if qry.length >= 1 then
@@ -115,7 +115,7 @@ class PagesController < ApplicationController
 
   def get_schedule_btn_class
     btn_class = "success"
-    if has_scheduled? then
+    if has_scheduled?
       btn_class = "secondary disabled" 
     end
     return btn_class
